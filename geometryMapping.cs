b9830561d5ab4cb7d613d4ab275a002cd7a757b3
jeanmarc2019 <jmdp1337@gmail.com>
2020-12-02 15:19:07 -0500

    clean up and changing geometry at runtime

--- geometryMapping.cs
+++ geometryMapping.cs
@@ -6,10 +6,11 @@ namespace GeometryMapper
 {
     class PhysicsHelper
     {
-        public static string currentGeometry = "Half-Space"; // default is Half-Space geometry
+        public static string currentGeometry = "Euclidean"; // default is Half-Space geometry
 
         public static void changeGeometry(String geometry)
         {
+            Debug.Log("Geometry set to " + geometry);
             currentGeometry = geometry;
         }
 
@@ -22,7 +23,7 @@ namespace GeometryMapper
             );
             return normalSum;
         }
-        private static Vector3 RussianArrowFromOrigin(float r, float varphi, float gamma, float t)
+        private static Vector3 nilGeodesicEqn(float r, float varphi, float gamma, float t)
         {
             float x = (r/(2*gamma))*(Mathf.Sin(2*gamma*t + varphi) - Mathf.Sin(varphi));
             float y = ((1+Mathf.Pow(gamma, 2f))/(2*gamma))*t - ((1 - Mathf.Pow(gamma, 2f))/(4*Mathf.Pow(gamma, 2f)))*Mathf.Sin(2*gamma*t);
@@ -30,32 +31,13 @@ namespace GeometryMapper
             Vector3 output = new Vector3(x,y,z);
             return output;
         }
-        public static float aFromDirection(Vector3 direction)
-        {
-            return (float)Math.Pow(Mathf.Pow(direction.x, 2f) + Mathf.Pow(direction.z, 2f), 0.5f);
-        }
-        public static float angleFromDirection(Vector3 direction)
-        {
-            return (float)Mathf.Atan2(direction.z, direction.x);
-        }
-        public static Vector3 ArrowFromOrigin(Vector3 input, float t)
-        {
-            return RussianArrowFromOrigin(aFromDirection(input), angleFromDirection(input), input.y, t);
-        }
-        private static Vector3 BringVelocityToOrigin(Vector3 velocity, Vector3 oldLocation)
-        {
-            return NilMultiply(-1*oldLocation, oldLocation + velocity);
-        }
-        private static Vector3 BeingVelocityToLocation(Vector3 velocity, Vector3 newLocation)
-        {
-            return -1*newLocation + NilMultiply(newLocation, velocity);
-        }
         private static Vector3 getRotationAxis(Vector3 input) {
             Vector2 projected = new Vector2(input.x, input.z);
             Vector2 orthogonal = Vector2.Perpendicular(projected);
             return new Vector3(orthogonal.x, 0, orthogonal.y);
         }
-        private static ComplexNum FLTGamma(float theta, float time) {
+        private static ComplexNum FLTGamma(float theta, float time)
+        {
             ComplexNum numerator = new ComplexNum(
                 Mathf.Exp(time)*Mathf.Cos(theta/2),
                 Mathf.Sin(theta/2)
@@ -72,12 +54,13 @@ namespace GeometryMapper
 
             switch(currentGeometry) {
                 case "Nil":
+                    Vector3 velocityAtOrigin = NilMultiply(-1*whereImShootingFrom, whereImShootingFrom + whatDirectionImShooting);
+                    float r = (float)Math.Pow(Mathf.Pow(velocityAtOrigin.x, 2f) + Mathf.Pow(velocityAtOrigin.z, 2f), 0.5f);
+                    float varphi = (float)Mathf.Atan2(velocityAtOrigin.z, velocityAtOrigin.x);
+                    float gamma = velocityAtOrigin.y;
                     return NilMultiply(
                         whereImShootingFrom,
-                        ArrowFromOrigin(
-                            BringVelocityToOrigin(whatDirectionImShooting, whereImShootingFrom),
-                            t
-                        )
+                        nilGeodesicEqn(r, varphi, gamma, t)
                     );
                 case "Half-Space":
                     float phi = Mathf.Atan2(whatDirectionImShooting.z, whatDirectionImShooting.x);
@@ -85,7 +68,6 @@ namespace GeometryMapper
                     ComplexNum geodesic = FLTGamma(
                         theta, t
                     );
-
                     Vector3 positionMapped = new Vector3(
                         Mathf.Cos(phi)*geodesic.r*whereImShootingFrom.y + whereImShootingFrom.x,
                         geodesic.i*whereImShootingFrom.y,
